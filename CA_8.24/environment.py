33b680755d41b8b4dda1e8c23f24021a59491168
absurd <117960088+LX-530@users.noreply.github.com>
2026-06-21 22:57:12 +0800

    Add unified environment compatibility entrypoint

---
+++ CA_8.24/environment.py
@@ -0,0 +1,13 @@
+from robot_env import (
+    EvacuationEnv,
+    RobotEnvConfig,
+    RobotEnvironment,
+    evacuation_env_creator,
+)
+
+__all__ = [
+    "EvacuationEnv",
+    "RobotEnvConfig",
+    "RobotEnvironment",
+    "evacuation_env_creator",
+]
